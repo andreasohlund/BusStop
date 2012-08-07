13d255e11d4d6914a2c89ccb77a42a0d25024552
Andreas Öhlund <andreasohlund2@gmail.com>
2012-08-07 16:17:55 +0200

    Added a orders controller and code to send the place order command

---
+++ BusStop.Api/Controllers/Default1Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusStop.Contracts;
+using NServiceBus;
+
+namespace BusStop.Api.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        public IBus Bus { get; set; }
+
+        public Guid Get()
+        {
+            var order = new PlaceOrder
+                {
+                    OrderId = Guid.NewGuid(),
+                    ProductId = Guid.NewGuid(),
+                    CustomerId = Guid.NewGuid(),
+                };
+
+            Bus.Send(order);
+
+            return order.OrderId;
+        }
+    }
+}
--- BusStop.Contracts/PlaceOrder.cs
+++ BusStop.Contracts/PlaceOrder.cs
@@ -8,6 +8,8 @@ namespace BusStop.Contracts
 {
     public class PlaceOrder : IMessage
     {
+        public Guid OrderId { get; set; }
+        
         public Guid ProductId { get; set; }
 
         public Guid CustomerId { get; set; }
