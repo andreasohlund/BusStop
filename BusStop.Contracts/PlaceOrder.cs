19271fb22f1dfd58d67eed003dbe98783190d251
Andreas Öhlund <andreasohlund2@gmail.com>
2012-08-07 15:34:29 +0200

    Added the PlaceOrder message contract

---
+++ BusStop.Contracts/PlaceOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NServiceBus;
+
+namespace BusStop.Contracts
+{
+    public class PlaceOrder : IMessage
+    {
+        public Guid ProductId { get; set; }
+
+        public Guid CustomerId { get; set; }
+    }
+}
