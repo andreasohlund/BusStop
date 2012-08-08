b0711d75adc1730cf200d347483beb5cad49ba25
Andreas Öhlund <andreasohlund2@gmail.com>
2012-08-08 15:44:36 +0200

    Added the backend host and tested the full message chain

---
+++ BusStop.Backend/EndpointConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace BusStop.Backend
+{
+    public class EndpointConfig:IConfigureThisEndpoint,AsA_Server
+    {
+    }
+}
---
+++ BusStop.Backend/PlaceOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+using BusStop.Contracts;
+
+namespace BusStop.Backend
+{
+    public class PlaceOrderHandler:IHandleMessages<PlaceOrder>
+    {
+        public void Handle(PlaceOrder message)
+        {
+            Console.WriteLine("Order received " + message.OrderId);
+        }
+    }
+}
