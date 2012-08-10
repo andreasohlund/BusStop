4c6ea3ae4ee16f168d5cd16608a76f1b7e7f58fa
Andreas Öhlund <andreasohlund2@gmail.com>
2012-08-10 14:35:33 +0200

    Orders are now saved in RavenDB

--- BusStop.Backend/PlaceOrderHandler.cs
+++ BusStop.Backend/PlaceOrderHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NServiceBus;
 using BusStop.Contracts;
+using Raven.Client.Document;
 
 namespace BusStop.Backend
 {
@@ -11,7 +12,31 @@ namespace BusStop.Backend
     {
         public void Handle(PlaceOrder message)
         {
+            var store = new DocumentStore 
+            { 
+                Url = "http://localhost:8080"
+            };
+
+            store.Initialize();
+            store.JsonRequestFactory.DisableRequestCompression = true;
+
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Order
+                {
+                    OrderId = message.OrderId
+                });
+
+                session.SaveChanges();
+            }
+
+
             Console.WriteLine("Order received " + message.OrderId);
         }
+    }
+
+    public class Order
+    {
+        public Guid OrderId { get; set; }
     }
 }
