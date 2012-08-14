991612a5b95b4095750dad5d26adddbf2c2f03cb
Andreas Öhlund <andreasohlund2@gmail.com>
2012-08-14 11:23:02 +0200

    Added a config section override for the error q

---
+++ BusStop.Config/ErrorQueueOveride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+
+namespace BusStop.Config
+{
+    public class ErrorQueueOveride : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "BusStop.Error"
+            };
+        }
+    }
+}
